21e6df4c72b8c811a62c767a117856c9325e1673
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-21 16:53:30 -0600

    Added the end-point for getting a single dish from the database

--- routes/dishes/dishes-router.js
+++ routes/dishes/dishes-router.js
@@ -10,7 +10,22 @@ router.get('/', async (req, res) => {
         res.status(200).json(dishes);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ error: 'The Dishes could not be retrieved' });
+        res.status(500).json({ error: 'The Dishes information could not be retrieved' });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dish = await Dishes.getDish(req.params.id);
+
+        if(dish) {
+            res.status(200).json(dish);
+        } else {
+            res.status(404).json({ error: 'That Dish id does not exist' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'The Dish information could not be retrieved' });
     }
 })
 module.exports = router;
