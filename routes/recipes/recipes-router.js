037d136428e6cbb41f94f7f69e5fd3136a1a29ed
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-02-21 17:07:24 -0600

    Added the end-point for adding a recipe to the database

--- routes/recipes/recipes-router.js
+++ routes/recipes/recipes-router.js
@@ -12,5 +12,19 @@ router.get('/', async (req, res) => {
         console.log(error);
         res.status(500).json({ error: 'The Recipes could not be retrieved' });
     }
+});
+
+router.post('/', async (req, res) => {
+    if(!req.body.name || !req.body.dishId || !req.body.instructions){
+        res.status(400).json({ error: 'The Recipe is missing its name, dishId or instructions. Please add them.' });
+    } else {
+        try {
+            const recipe = await Recipes.addRecipe(req.body);
+            res.status(200).json(recipe);
+        } catch (error) {
+            console.log(error);
+            json.status(500).json({ error: 'Could not add the Recipe to the database' });
+        }
+    }
 })
 module.exports = router;
